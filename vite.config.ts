283b1b624d448bcb437c6a8375ee85e0687830a3
kryukovaolga <olgak.kryukova@gmail.com>
2025-02-09 14:15:47 +0100

    chore: add ts path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { configDefaults } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   test: {
     globals: true, // Enables global test and expect
     environment: "jsdom", // Simulates browser-like environment
