3a7711276f551ce47b0b2f32102071db55082348
kryukovaolga <olgak.kryukova@gmail.com>
2025-02-09 14:19:36 +0100

    style: customize theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import App from "./components/App/App";
+import { system } from "@/components/ui/theme";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ChakraProvider value={defaultSystem}>
+    <ChakraProvider value={system}>
       <App />
     </ChakraProvider>
   </StrictMode>
